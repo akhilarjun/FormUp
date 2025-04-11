35ecd9facf470813ac4a507fe376f467cafee940
Akhil Arjun <akhilparjun@gmail.com>
2025-04-11 20:20:55 +0530

    FIX: Fixed proper config for loading typescript config of lib

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), dts({ include: ["lib"] })],
+	plugins: [
+		react(),
+		dts({ tsconfigPath: resolve(__dirname, "tsconfig.lib.json") }),
+	],
 	build: {
 		copyPublicDir: false,
 		lib: {
