82f8921b5457504b09ae7ea250c784258f0da0d7
Akhil Arjun <akhilparjun@gmail.com>
2025-06-05 00:00:24 +0530

    Prettier Fix

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,25 +1,29 @@
-import { FC, useState } from "react";
-import "../styles.css";
-import { randomId } from "../utilities";
+import { FC, useState } from 'react';
+import '../styles.css';
+import { randomId } from '../utilities';
 
-type InputComponentProps = {value:string, label: string, type: string};
+type InputComponentProps = { value: string; label: string; type: string };
 
-export const InputComponent : FC<InputComponentProps> = ({ value, label, type }:InputComponentProps) => {
-	const [val, setVal] = useState(value);
-	const rdId = randomId();
-    console.log(`Input component - ${label} rendering`, Date.now())
-	return (
-		<>
-			<div className="input-container-formup">
-				<label htmlFor={rdId}>{label}</label>
-				<input
-					className="formup-1234"
-					type={type}
-					id={rdId}
-					value={val}
-					onChange={(e) => setVal(e.target.value)}
-				/>
-			</div>
-		</>
-	);
+export const InputComponent: FC<InputComponentProps> = ({
+    value,
+    label,
+    type,
+}: InputComponentProps) => {
+    const [val, setVal] = useState(value);
+    const rdId = randomId();
+    console.log(`Input component - ${label} rendering`, Date.now());
+    return (
+        <>
+            <div className="input-container-formup">
+                <label htmlFor={rdId}>{label}</label>
+                <input
+                    className="formup-1234"
+                    type={type}
+                    id={rdId}
+                    value={val}
+                    onChange={(e) => setVal(e.target.value)}
+                />
+            </div>
+        </>
+    );
 };
